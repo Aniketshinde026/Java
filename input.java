0c43674c5dd15d4bd4ba08efb010b3708d38f6a0
Aniket Shinde <shindeaniket2630@gmail.com>
2025-05-26 00:28:27 +0530

    .

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class input{
+    public static void main(String[] args) {
+        System.out.print("Enter the Name:");
+        String name;
+        Scanner sc= new Scanner(System.in);
+        name=sc.next();
+        //nextLine()
+        //nextInt
+        //nextDouble
+
+        System.out.print(name);
+
+    }
+}
---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sum{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Value of A: ");
+        int a=sc.nextInt();
+        System.out.println("Enter Value of B: ");
+        int b=sc.nextInt();
+
+        int sum=a+b;
+
+        System.out.print(sum);
+    }
+}
