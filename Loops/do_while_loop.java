4765c01b871c41bb5c9dd0518cca622be12adb70
Aniket Shinde <shindeaniket2630@gmail.com>
2025-05-26 22:57:01 +0530

    .

---
+++ Loops/do_while_loop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class do_while_loop{
+    public static void main(String[] args){
+        int i=1;
+        do{
+            System.out.println(i);
+            i=i+1;
+        }while(i<11);
+    }
+}
