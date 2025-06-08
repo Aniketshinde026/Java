3ad739dc2fc97c751aecf09e2a38ba1863646c35
Aniket Shinde <shindeaniket2630@gmail.com>
2025-06-08 23:18:06 +0530

    .

---
+++ Arrays/arrays.java
@@ -0,0 +1,16 @@
+class arrays{
+
+    public static void main(String[] args) {
+        int[] marks=new int[4];
+        marks[0]=98;
+        marks[1]=97;
+        marks[2]=95;
+        marks[3]=94;
+
+        System.out.println(marks[0]);
+         System.out.println(marks[1]);
+          System.out.println(marks[2]);
+           System.out.println(marks[3]);
+        
+    }
+}
